f0f525af5ab289e74e7f1fc7c1b17c6af245df30
ifBars <114284668+ifBars@users.noreply.github.com>
2023-05-07 15:40:57 -0600

    Revert "v0.0.6"
    This reverts commit 280ebd1ed2792d8259fe00620711e6558df6de34.

--- StringMatchingTools/SMT.cs
+++ StringMatchingTools/SMT.cs
@@ -4,15 +4,13 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Numerics;
-using System.Collections;
 
 namespace StringMatchingTools
 {
     public static class SMT
     {
 
-        private static readonly IReadOnlyCollection<string> CommonWords = new[]
+        private static readonly HashSet<string> CommonWords = new HashSet<string>
         {
             "the", "and", "a", "to", "in", "that", "it", "with", "as", "for", "was", "on", "are", "be", "by", "at",
             "an", "this", "who", "which", "or", "but", "not", "is", "error", "can", "were", "been", "being", "one",
@@ -22,25 +20,67 @@ namespace StringMatchingTools
 
         public static string ExtractKeywords(this string input, int amt, string path = null)
         {
-            IReadOnlyList<string> preferredWords = new string[0];
+
+            List<string> preferredWords = new List<string>();
             if (path != null)
             {
                 // Read words from the text file and store them in a list
-                preferredWords = File.ReadAllLines(path);
+                preferredWords = File.ReadAllLines(path).ToList();
             }
 
             // Split string into words
             var words = input.Split(' ')
                 // Remove common words from string
-                .Where(w => !CommonWords.Contains(w.ToLowerInvariant()))
+                .Where(w => !CommonWords.Contains(w.ToLowerInvariant()));
+
                 // If preferred words are provided, take them first
-                .OrderByDescending(w => preferredWords.Contains(w.ToLowerInvariant()))
-                // Take the specified amount of keywords
-                .Take(amt);
+            if (preferredWords.Count > 0)
+            {
+                words = words
+                    .Where(w => preferredWords.Contains(w.ToLowerInvariant()))
+                    .Concat(words.Where(w => !preferredWords.Contains(w.ToLowerInvariant())));
+            }
+
+            // Take the specified amount of keywords
+            words = words.Take(amt);
 
             // Return Keywords
             return string.Join(" ", words);
         }
+
+            private static int Calculate(string source1, string source2)
+            {
+                var source1Length = source1.Length;
+                var source2Length = source2.Length;
+
+                var matrix = new int[source1Length + 1, source2Length + 1];
+
+                // First calculation, if one entry is empty return full length
+                if (source1Length == 0)
+                    return source2Length;
+
+                if (source2Length == 0)
+                    return source1Length;
+
+                // Initialization of matrix with row size source1Length and columns size source2Length
+                for (var i = 0; i <= source1Length; matrix[i, 0] = i++) { }
+                for (var j = 0; j <= source2Length; matrix[0, j] = j++) { }
+
+                // Calculate rows and collumns distances
+                for (var i = 1; i <= source1Length; i++)
+                {
+                    for (var j = 1; j <= source2Length; j++)
+                    {
+                        var cost = (source2[j - 1] == source1[i - 1]) ? 0 : 1;
+
+                        matrix[i, j] = Math.Min(
+                            Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1),
+                            matrix[i - 1, j - 1] + cost);
+                    }
+                }
+                // return result
+                return matrix[source1Length, source2Length];
+            }
 
         private static string Preprocess(string input)
         {
@@ -57,76 +97,24 @@ namespace StringMatchingTools
             return string.Join(" ", words);
         }
 
-        public static double Check(string uInput, string uInput2, bool preProcess)
+        public static double Check(string uInput, string uInput2, bool preProccess)
         {
-            if (preProcess)
+            if (preProccess)
             {
+                // Preprocess the inputs
                 uInput = Preprocess(uInput);
                 uInput2 = Preprocess(uInput2);
             }
 
-            int length = uInput.Length;
-            int length2 = uInput2.Length;
-            int minLength = Math.Min(length, length2);
-
-            // Prepare a vector for the substitution cost of matching characters
-            Vector<byte> match = new Vector<byte>((byte)1);
-
-            // Prepare vectors for the insertion and deletion costs
-            Vector<byte> insertion = new Vector<byte>((byte)1);
-            Vector<byte> deletion = new Vector<byte>((byte)1);
-
-            // Initialize the distances array
-            int[] distances = new int[length2 + 1];
-
-            for (int i = 0; i <= length2; i++)
-            {
-                distances[i] = i;
-            }
-
             // Use parallelism to perform the calculation
-            Parallel.For(0, length, i =>
-            {
-                int previousDistance = i;
-                byte c1 = (byte)uInput[i];
+            var task1 = Task.Factory.StartNew(() => Calculate(uInput, uInput2));
+            var task2 = Task.Factory.StartNew(() => Calculate(uInput2, uInput));
+            Task.WaitAll(task1, task2);
 
-                // Calculate the distances for the current row using SIMD instructions
-                for (int j = 0; j < length2; j += Vector<byte>.Count)
-                {
-                    // Load a vector of characters from the second string
-                    Vector<byte> v2 = new Vector<byte>(uInput2.Substring(j, Vector<byte>.Count).PadRight(Vector<byte>.Count, '\0').Select(c => (byte)c).ToArray());
+            int distance = Math.Min(task1.Result, task2.Result);
+            double similarity = 1.0 - (double)distance / Math.Max(uInput.Length, uInput2.Length);
 
-                    // Compare the vectors to find matching characters
-                    Vector<byte> mask = Vector.Equals(v2, new Vector<byte>(c1));
-
-                    // Calculate the substitution costs using the mask
-                    Vector<byte> vMatch = Vector.BitwiseAnd(mask, match);
-                    Vector<byte> vSubstitution = Vector.Add(vMatch, Vector.Subtract(Vector<byte>.One, mask));
-
-                    // Calculate the insertion and deletion costs
-                    Vector<byte> vInsertion = new Vector<byte>(insertion) + Vector<byte>.One;
-                    Vector<byte> vDeletion = new Vector<byte>(deletion) + Vector<byte>.One;
-
-                    // Calculate the new distances vector using the minimum of the costs
-                    Vector<byte> vDistances = Vector.Min(Vector.Min(vInsertion, vDeletion), vSubstitution);
-
-                    // Store the new distances vector in the distances array
-                    byte[] distancesArray = new byte[Vector<byte>.Count];
-                    vDistances.CopyTo(distancesArray);
-                    for (int k = 0; k < Vector<byte>.Count; k++)
-                    {
-                        distances[j + k + 1] = Math.Min(Math.Min(distances[j + k + 1], distances[j + k] + 1), previousDistance + distancesArray[k]);
-                    }
-
-                    // Update the previous distance
-                    previousDistance = distances[j + Vector<byte>.Count];
-                }
-
-                // Update the last element of the distances array
-                distances[length2] = Math.Min(distances[length2], previousDistance + 1);
-            });
-
-            double similarity = 1.0 - (double)distances[length2] / Math.Max(uInput.Length, uInput2.Length);
+            // Return similarity between strings
             return similarity;
         }
 
